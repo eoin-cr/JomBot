fb2482c2c6c8b74c22d419689f79b16a6a52ae0b
Eoin <79023828+eoin-cr@users.noreply.github.com>
2022-03-18 14:30:54 +0000

    Changed timezone functionality to use a json file instead of a txt file

--- cogs/time.py
+++ cogs/time.py
@@ -3,6 +3,8 @@ import discord
 from discord.ext import commands
 from datetime import datetime
 import bot as main
+import json
+import os
 
 
 # TODO: use better file stuff than just a txt file - json file perhaps
@@ -45,44 +47,49 @@ class Time(commands.Cog):
             # print(area)
             area = int(area.strip('@,<>,!,()'))
             print(area)
-            with open('timezones.txt') as f:
-                # data = f.read()
 
-                # print(f'data: {data}')
-                # print(f'data1: {data[1]}')
-                for line in f:
-                    # print(f'line: {line}')
-                    line = line.split(" ")
-                    # print(f'line0: {line[0]}')
-                    # print(f'area: {area}')
-                    if line[0] == str(area):
-                        print("Entering!")
-                        hour = int(now.strftime("%H")) + int(line[1])
-                        if hour >= 24:
-                            hour -= 24
-                        elif hour < 0:
-                            hour += 24
-                        time = "" + str(hour) + str(now.strftime(":%M:%S"))
-                        memb = await ctx.guild.fetch_member(area)
-                        embed = discord.Embed(color=discord.Color.blue())
-                        embed.add_field(name="Time", value=f"The time for {memb.mention} is {time}", inline=False)
-                        embed.set_author(name=memb.display_name, icon_url=memb.avatar_url)
-                        return await ctx.send(embed=embed)
-                        # return await message.channel.send(time)
+            if not os.path.exists("timezones.json"):
+                with open("timezones.json", "w") as f:
+                    json.dump({}, f)
 
+            with open("timezones.json") as f:
+                data = json.load(f)
+
+            print(area)
+            print(data)
+            print(data.get(f'{area}'))
+            if data.get(f'{area}') is None:
                 memb = await ctx.guild.fetch_member(area)
                 embed = discord.Embed(color=discord.Color.purple())
                 embed.add_field(name="Time", value="No timezone found for that user", inline=False)
                 embed.set_author(name=memb.display_name, icon_url=memb.avatar_url)
                 return await ctx.send(embed=embed)
 
+            # Else statement isn't necessary due to the return in the if but it's more obvious
+            # what the code means when quickly glancing at the code
+            else:
+                print("hi")
+                hour = int(now.strftime("%H")) + int(data.get(f'{area}'))
+                print(hour)
+                if hour >= 24:
+                    hour -= 24
+                elif hour < 0:
+                    hour += 24
+                time = "" + str(hour) + str(now.strftime(":%M:%S"))
+                memb = await ctx.guild.fetch_member(area)
+                embed = discord.Embed(color=discord.Color.blue())
+                embed.add_field(name="Time", value=f"The time for {memb.mention} is {time}", inline=False)
+                embed.set_author(name=memb.display_name, icon_url=memb.avatar_url)
+                print("hello")
+                return await ctx.send(embed=embed)
+                # return await message.channel.send(time)
+
     @commands.command(name="set-time", aliases=["set time", "time-set"],
-                      help="Sets your timezone (use UTC+/-X)")
+                      help="Sets a user's timezone (use UTC+/-X)")
     async def set_time(self, message, zone, *user):
-        add = ""
         if user is None:
             user = message.author.id
-            print("NONE!")
+            # print("NONE!")
         else:
             temp = user
             user = zone
@@ -95,8 +102,19 @@ class Time(commands.Cog):
             embed = main.embed_func(message, "Set time", "Invalid timezone!  Set a time in the "
                                                          "format UTC+/-X.", discord.Color.red())
             return await message.channel.send(embed=embed)
-        with open('timezones.txt', 'a') as f:
-            f.write(f'\n{user} {zone}')
+
+        if not os.path.exists("timezones.json"):
+            with open("timezones.json", "w") as f:
+                json.dump({}, f)
+
+        with open("timezones.json") as f:
+            data = json.load(f)
+
+        data[user] = zone
+
+        with open("timezones.json", "w") as f:
+            json.dump(data, f)
+
         memb = await message.guild.fetch_member(user)
         embed = main.embed_func(message, "Set time", f"{memb.mention}'s timezone has been set as UTC{zone}!")
         await message.channel.send(embed=embed)
