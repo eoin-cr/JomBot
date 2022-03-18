7a1710bc0476efdd3024986e254229454a1792ee
Eoin <79023828+eoin-cr@users.noreply.github.com>
2022-03-18 14:36:25 +0000

    Changed timezone functionality to use a json file instead of a txt file

--- cogs/time.py
+++ cogs/time.py
@@ -46,7 +46,7 @@ class Time(commands.Cog):
         else:
             # print(area)
             area = int(area.strip('@,<>,!,()'))
-            print(area)
+            # print(area)
 
             if not os.path.exists("timezones.json"):
                 with open("timezones.json", "w") as f:
@@ -55,9 +55,8 @@ class Time(commands.Cog):
             with open("timezones.json") as f:
                 data = json.load(f)
 
-            print(area)
-            print(data)
-            print(data.get(f'{area}'))
+            # Note: data.get(area) does not work, it has to be data.get(f'{area}')
+            # Check to see if there's any data stored for that user
             if data.get(f'{area}') is None:
                 memb = await ctx.guild.fetch_member(area)
                 embed = discord.Embed(color=discord.Color.purple())
@@ -68,13 +67,15 @@ class Time(commands.Cog):
             # Else statement isn't necessary due to the return in the if but it's more obvious
             # what the code means when quickly glancing at the code
             else:
-                print("hi")
                 hour = int(now.strftime("%H")) + int(data.get(f'{area}'))
-                print(hour)
+
+                # Makes sure the displayed hour is between 0 and 24
                 if hour >= 24:
                     hour -= 24
                 elif hour < 0:
                     hour += 24
+
+                # Creates time string
                 time = "" + str(hour) + str(now.strftime(":%M:%S"))
                 memb = await ctx.guild.fetch_member(area)
                 embed = discord.Embed(color=discord.Color.blue())
@@ -91,27 +92,37 @@ class Time(commands.Cog):
             user = message.author.id
             # print("NONE!")
         else:
+            # Converts a tagged user string into a user id
             temp = user
             user = zone
             zone = temp
             user = int(user.strip('@,<>,!,()'))
 
         zone = ''.join(zone)
+        # Removes "utc" from the string.  We could tell the user to just enter the value without
+        # putting utc in front of it and avoid this, but I believe that is likely to lead to
+        # confusion.
         zone = zone.lower().replace("utc", "")
+
+        # Makes sure the timezone is a valid one
         if int(zone) < -12 or int(zone) > 12:
             embed = main.embed_func(message, "Set time", "Invalid timezone!  Set a time in the "
                                                          "format UTC+/-X.", discord.Color.red())
             return await message.channel.send(embed=embed)
 
+        # If timezones.json does not exist, create it
         if not os.path.exists("timezones.json"):
             with open("timezones.json", "w") as f:
                 json.dump({}, f)
 
+        # Open the timezones.json file and read the data to `data`
         with open("timezones.json") as f:
             data = json.load(f)
 
+        # Write the zone to the user data
         data[user] = zone
 
+        # Write this data to the file
         with open("timezones.json", "w") as f:
             json.dump(data, f)
 
