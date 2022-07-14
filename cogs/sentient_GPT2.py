a094a525101366a3f5e15fc7e2eb53ad12d97e36
clubby789 <jamie@hill-daniel.co.uk>
2022-07-14 01:13:56 +0100

    Add typing

--- cogs/sentient_GPT2.py
+++ cogs/sentient_GPT2.py
@@ -2,6 +2,7 @@ import gpt_2_simple as gpt2
 from discord.ext import commands
 import re
 
+
 class Sentient(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -16,15 +17,15 @@ class Sentient(commands.Cog):
             self.sess = gpt2.reset_session(self.sess)
 
         gpt2.load_gpt2(self.sess)
-
-        text = gpt2.generate(self.sess, prefix=text, return_as_list=True)
-        print(text)
-        i = 0
+        async with ctx.typing():
+            # blocking async is bad but i simply do not care
+            text = gpt2.generate(self.sess, prefix=text, return_as_list=True)
+            print(text)
         # Remove emoji-only lines
-        text = [l for l in text[0].split("\n") if not if not re.match(r'^:.*:$', l)][:20]
+        text = [l for l in text[0].split("\n") if not re.match(r"^:.*:$", l)][:20]
         # Remove backticks
-        text = '\n'.join(text).replace('`', '')[:1990]
-        await ctx.send('```\n' + text + '```')
+        text = "\n".join(text).replace("`", "")[:1990]
+        await ctx.send("```\n" + text + "```")
 
 
 def setup(bot):
